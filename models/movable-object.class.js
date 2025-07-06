5a48011a7c8ece084684a7b61e3f4ae3a7404b6a
Tim Swodenk <tim.swodenk@web.de>
2025-07-06 10:43:10 +0200

    added frame around objects

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -30,6 +30,18 @@ class MovableObject {
     this.img.src = path;
   }
 
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = "4";
+    ctx.strokeStyle = "red";
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
+
   /**
    *
    * @param {Array} arr - ['img/image1.png', 'img2.png', ..]
