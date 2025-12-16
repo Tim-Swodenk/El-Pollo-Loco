c2f01c276e61d9b7fadea5a94361a2c04296f189
Tim Swodenk <tim.swodenk@web.de>
2025-12-16 09:54:30 +0100

    Refactor status bars to use unified StatusBar class
    Removed individual status bar subclasses and replaced them with a single configurable StatusBar class. Updated world initialization to use configuration objects for each status bar type, simplifying code and reducing duplication.

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -5,18 +5,29 @@
 class StatusBar extends DrawableObject {
   /**
    * Creates a new status bar.
-   * @param {string[]} images - Image paths for different percentage states.
-   * @param {number} percentage - Initial percentage value (0 to 100).
-   * @param {number} y - Vertical position of the bar.
+   * @param {Object} config - Configuration for the bar visuals and position.
+   * @param {string[]} config.images - Image paths for different percentage states.
+   * @param {number} [config.percentage=0] - Initial percentage value (0 to 100).
+   * @param {number} [config.x=10] - Horizontal position of the bar.
+   * @param {number} [config.y=0] - Vertical position of the bar.
+   * @param {number} [config.width=180] - Display width of the bar.
+   * @param {number} [config.height=50] - Display height of the bar.
    */
-  constructor(images, percentage, y) {
+  constructor({
+    images,
+    percentage = 0,
+    x = 10,
+    y = 0,
+    width = 180,
+    height = 50,
+  }) {
     super();
     this.IMAGES = images;
     this.loadImages(this.IMAGES);
-    this.x = 10;
+    this.x = x;
     this.y = y;
-    this.width = 180;
-    this.height = 50;
+    this.width = width;
+    this.height = height;
     this.setPercentage(percentage);
   }
 
